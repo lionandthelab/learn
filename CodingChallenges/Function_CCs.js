76a2a39a1244735d1988e44bf2351003022044c8
issac <chldltkrz@gmail.com>
2021-08-23 14:26:31 +0900

    removed comments in practices and a challenge
    	It was hard to recognize what I practiced because major part of the file was commented out in Function_CCs.js file
    	Now I commented out what was commented, and lint the code little bit to increase its readability
    
    	Practiced concepts are:
    	    1. Default Parameters and variable arguments
    	    2. Pass by value and Reference
    	    3. Callback & Higher Order Function
    	    4. array function
    	    5. Call, Bind, Apply methods
    	    6. IIFE
    
    	Since there are advanced topic in this chapter, it is highly
    	recommended to revisit this chapter later.
    
    Signed-off-by: issac <chldltkrz@gmail.com>

--- CodingChallenges/Function_CCs.js
+++ CodingChallenges/Function_CCs.js
@@ -1,5 +1,144 @@
 "use strict";
+// Assume following object is existing
+const booking = [];
 
+// setting default parameters
+const createBooking = function (flightNum, numPassenger = 1, price = 199) {
+  const bookingObj = {
+    flightNum,
+    numPassenger,
+    price,
+  };
+  console.log(bookingObj);
+  booking.push(bookingObj);
+};
+
+createBooking("LH123");
+createBooking("LH123", 2, 800);
+//____________________________________________
+// How Passing Arguments Works: Values vs. Reference
+// JS only has pass by reference
+const flight = "LH234";
+const issac = {
+  name: "issac Choi",
+  passport: 111,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = "HF123";
+  passenger.name = "MR." + passenger.name;
+  if (passenger.passport === 111) {
+    alert("Checked in");
+  } else {
+    alert("wrong passport");
+  }
+};
+
+checkIn(flight, issac);
+console.log(flight);
+console.log(issac);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000);
+};
+
+newPassport(issac);
+checkIn(flight, issac);
+console.log(flight);
+console.log(issac);
+//____________________________________________
+
+// // CALLBACK && HIger Order Function Example
+const oneWord = function (str) {
+  return str.replace(/ /g, "").toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(" ");
+  return [first.toUpperCase(), ...others].join(" ");
+};
+//Higher-order Function because it takes other function as argument
+const transformer = function (str, fn) {
+  console.log(`Transformed. string : ${fn(str)}`);
+  console.log(`Transformed by ${fn.name}`);
+};
+
+//invoking value
+transformer("JavaScript is the best ", upperFirstWord);
+transformer("JavaScript is the best ", oneWord);
+//____________________________________________
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} : ${name}`);
+  };
+};
+
+const greeterHey = greet("HEY");
+greet("hi")("ahdd");
+
+//using arrow function
+const hey = (hello) => (name) => console.log(`${hello} : ${name}`);
+hey("hwiwiwiwiw")("wueh");
+//____________________________________________
+const lufthansa = {
+  airline: "Lufthansa",
+  iataCode: "LH",
+  bookings: [],
+  book: function (flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push([`flight: ${this.iataCode}${flightNum}`]);
+  },
+};
+
+lufthansa.book(1234, "ahriri");
+console.log(lufthansa);
+const eurowings = {
+  airline: "Eurowings",
+  iataCode: "EW",
+  bookings: new Array(),
+};
+
+const book = lufthansa.book;
+
+//below will occur an error because it is pointing undefined
+//book(23, 'issac choi');
+//call apply bind!
+//call takes first target point(target this object) and rest of arguments
+book.call(eurowings, 23, "issac choi");
+
+console.log(eurowings);
+
+//apply -> diff is apply does not take parameter but an array of paramaters
+
+//bind will simply set this keyword to the object
+const bookEW = book.bind(eurowings);
+bookEW(124123, "sample");
+// this will set first argument to 23(PRESET!!!)
+const bookEWs = book.bind(eurowings, 23);
+// function call won't need first argument!!!!
+bookEWs("sample");
+
+//with Event listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+
+  this.planes++;
+  console.log(this.planes);
+};
+document
+  .querySelector(".buy")
+  .addEventListener("click", lufthansa.buyPlane.bind(lufthansa));
+
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// same as-> addVat = value -> value + value * 0.23
+console.log(addVAT(100));
 //____________________________________________________________________________________________________________________________________
 
 // //CC1!
@@ -75,13 +214,25 @@ poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, "string");
 poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
 //____________________________________________________________________________________________________________________________________
 
-// //IIFE - when you need to execute a function only once and never use it again
-// // if wrap function in parenthesis, will recognize as expression
+//IIFE - when you need to execute a function only once and never use it again
+// if wrap function in parenthesis, will recognize as expression
 (function () {
   console.log("HIthere!");
 })();
 (() => console.log("thishtishtishtihsi"))();
 // //____________________________________________________________________________________________________________________________________
+
+//CLOSUREs!
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(passengerCount);
+  };
+};
+
+const bookier = secureBooking();
 
 ///////////////////////////////////////
 // Coding Challenge #2
