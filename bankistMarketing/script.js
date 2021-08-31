590d9019513866ae3ab5e31826f200d1e5563d8b
issac <chldltkrz@gmail.com>
2021-08-31 15:42:31 +0900

    Studying advanced DOM and Events
    	1. Learn various type of events - mouseenter, scroll, click.,, ...etc
            2. concept of Event Propagation: Bubbling, Capturing
            3. implementing Navigation Scolling feature using Event delegation extends concept of Bubbling,
    
    Signed-off-by: issac <chldltkrz@gmail.com>

--- bankistMarketing/script.js
+++ bankistMarketing/script.js
@@ -65,7 +65,7 @@ document.querySelector('.header').append(message);
 // duplicate the element!!
 document.querySelector('.header').append(message.cloneNode(true));
 
-//Delete elements
+// Delete elements
 document
   .querySelector('.btn--close-cookie')
   .addEventListener('click', function () {
@@ -143,4 +143,98 @@ btnScrollTo.addEventListener('click', function (e) {
 
   // scrolling3 -> set target and set effect!, but works with only modern browser.
   section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+// 210831 - Various Events
+// consult with MDN Event reference page to know mmore events
+
+const h1 = document.querySelector('h1');
+
+const alertH1 = function () {
+  alert('hi');
+  h1.removeEventListener('mouseenter', alertH1);
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+// another way to attach event to the element
+// this is old school way!
+h1.onmouseenter = function (e) {
+  alert('hello');
+};
+
+// diff btw addEventListener and property setting method
+// -> addEventListener
+//    can add multiple eventListener to one event
+//    can delete eventListener when needed
+
+// Bubbling and Capturing phase
+// Travel of Event - (https://ko.javascript.info/bubbling-and-capturing)
+// Capturing Phase -> Target Phase -> Bubbling Phase
+// Document ->        target       -> document
+// but not ALL Event has capturing and bubbling event
+
+// nav Coloring and add Functionality
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+
+  // to stop bubbling propagation(Not great idea in real world)
+  // e.stopPropagation();
+});
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+});
+document.querySelector('.nav').addEventListener(
+  'click',
+  function (e) {
+    this.style.backgroundColor = randomColor();
+    console.log('LINK', e.target, e.currentTarget);
+    console.log(e.currentTarget === this);
+  },
+  // when third argument is set to true, it is capturing not bubbling
+  true
+);
+
+// Event Delegation
+// Assume implement smooth scroll to nav menus to the corresponding section
+
+///////////////////////////////////////
+// Page Navigation
+
+// document.querySelectorAll('.nav__link').forEach(function (nav) {
+//   nav.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     console.log('Link');
+//     const id = this.getAttribute('href');
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+// what if we need to attach 10,000 eventhandler?? OMG< THERE IS Better solution, which is event delegation
+
+// -> Event Delegation
+// 1. add eventlistener to the common parent element
+// 2. Determine what elements originated the event
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  // e.target store the element originated the event
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
 });
