118b691234ee4388ae13c14b1bef88eb7a7bb6eb
Issac Choi <chldltkrz@gmail.com>
2021-09-13 20:31:22 +0900

    Prototype and Prototype inheritance and Chaning
    Signed-off-by: Issac Choi <chldltkrz@gmail.com>

--- OOP/script.js
+++ OOP/script.js
@@ -37,10 +37,37 @@ Person.prototype.calcAge = function () {
 jonas.calcAge();
 
 console.log(jonas.__proto__ === Person.prototype); //true
-console.log(Person.prototype.isPrototype(jonas)); //true
-console.log(Person.prototype.isPrototype(Person)); //false
+console.log(Person.prototype.isPrototypeOf(jonas)); //true
+console.log(Person.prototype.isPrototypeOf(Person)); //false
 
 Person.prototype.species = 'Homo Sapiens';
 console.log(jonas);
 console.log(jonas.hasOwnProperty('firstname')); //true
 console.log(jonas.hasOwnProperty('species')); //false -> because it just has access to prototype not in object
+
+// 210913 Prototype Inheritance and Chain
+/* 
+  Prototype Inehritance(delegation) -> Any properties that saved on __proto__ will be accessible throughout object Scope even though it is not defined in the object itself
+  In other word, if property is set to __proto__, then every Object made out of that prototype will have that property
+*/
+/*
+  Prototype has its parent Prototype, the Object Prototype
+  End of prototype chain is Null
+*/
+console.log(jonas.__proto__);
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+const arr = [1, 2, 33, 3, 3, 3, 3, 3, 3];
+
+console.log(arr.__proto__ === Array.prototype);
+
+// below will add functionality to all arr object
+// but DON DO IT for several reason
+//    1. MDN might will add same code
+//    2. Code will get messy in team project
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
