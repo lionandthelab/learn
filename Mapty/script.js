6941b8dbe88f373b3a51bee7f8e50f646627a12c
Issac Choi <chldltkrz@gmail.com>
2021-10-04 15:58:20 +0900

    Refactoring the existing code into class form
    Signed-off-by: Issac Choi <chldltkrz@gmail.com>

--- Mapty/script.js
+++ Mapty/script.js
@@ -12,24 +12,118 @@ const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
 // 210923 GEOLocation API is browser supporting API,
+// display map using Leaflet
 // two callbacks first on success, second on failure
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10); //
 
-navigator.geolocation.getCurrentPosition(
-  function (position) {
+  constructor(coords, distance, duration) {
+    // this.date = ...
+    // this.id = ...
+    this.coords = coords; // [lat, lng]
+    this.distance = distance; //in Km
+    this.duration = duration; // in min
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+  calcPace() {
+    // min/Km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+  calcSpeed() {
+    // Km/h
+    this.speed = this.distance / this.duration / 60;
+    return this.speed;
+  }
+}
+// test data
+// const run1 = new Running([39, -12], 5.2, 24, 167);
+// const cycling1 = new Cycling([39, -12], 27, 95, 523);
+
+///////////////////////////////////////////////////////////////////////////////////
+//Application Architecture
+class App {
+  #map;
+  #mapEvent;
+
+  constructor() {
+    this._getPosition();
+    form.addEventListener('submit', this._newWorkout.bind(this));
+    inputType.addEventListener('change', this._toggleElevationField);
+  }
+  _getPosition() {
+    navigator.geolocation.getCurrentPosition(
+      this._loadMap.bind(this),
+      function () {
+        alert('Can not get your POsition');
+      }
+    );
+  }
+  _loadMap(position) {
     const { latitude, longitude } = position.coords;
     const coords = [latitude, longitude];
-    var map = L.map('map').setView(coords, 13);
+
+    this.#map = L.map('map').setView(coords, 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
+    }).addTo(this.#map);
 
-    L.marker(coords).addTo(map).bindPopup('My Company').openPopup();
-  },
-  function () {
-    alert('Can not get your POsition');
+    // method from leaflet like addEventLisntener
+    this.#map.on('click', this._showForm.bind(this));
   }
-);
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+  _toggleElevationField() {
+    // change input field when we change the dropdown menu
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+  _newWorkout(e) {
+    e.preventDefault();
 
-// display map using Leaflet
+    // clearing input fields!
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
+
+    // DIsplay marker
+    const { lat, lng } = this.#mapEvent.latlng;
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        // consult with the official documentation of leaflet
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup',
+        })
+      )
+      .setPopupContent('working')
+      .openPopup();
+  }
+}
+const app = new App();
